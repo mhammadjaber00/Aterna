a44793fadf210a159fd1d1ca642d1e1ff4d96138
Mohammad Jaber <mohamad.jaber@neo.ae>
2025-08-31 20:13:35 +0300

    Update version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -112,8 +112,8 @@ android {
         applicationId = "io.yavero.aterna"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "0.1.0"
+        versionCode = 4
+        versionName = "0.1.1"
     }
     packaging {
         resources {
