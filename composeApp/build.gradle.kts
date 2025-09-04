48cac81281578cffe7db7145d1dcf39a438c8715
Mohammad Jaber <mohamad.jaber@neo.ae>
2025-09-05 00:33:28 +0300

    Update versionCode to 5 in build configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -119,7 +119,7 @@ android {
         applicationId = "io.yavero.aterna"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
+        versionCode = 5
         versionName = "0.1.2"
     }
     packaging {
